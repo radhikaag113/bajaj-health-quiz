d357f7cd431348952bed52adb05f000b84dfc01d
radhikaag113 <radhikaag113@gmail.com>
2026-04-24 19:02:23 +0530

    Update QuizLeaderboard.java

--- QuizLeaderboard.java
+++ QuizLeaderboard.java
@@ -13,23 +13,23 @@ import java.util.regex.Pattern;
 
 public class QuizLeaderboard {
 
-    // TODO: Change this to your actual registration number
+  
     private static final String REG_NO = "RA2311003030459";
     private static final String BASE_URL = "https://devapigw.vidalhealthtpa.com/srm-quiz-task";
 
     public static void main(String[] args) {
         HttpClient client = HttpClient.newHttpClient();
 
-        // HashSet to store unique combinations of "roundId_participant" to handle duplicates
+       
         Set<String> seenEvents = new HashSet<>();
 
-        // HashMap to store the total score for each participant
+       
         Map<String, Integer> totalScores = new HashMap<>();
 
         System.out.println("Starting 10 API polls...\n");
 
         try {
-            // Step A: Poll the API 10 times
+            
             for (int i = 0; i < 10; i++) {
                 System.out.println("Polling index " + i + "...");
 
@@ -41,8 +41,7 @@ public class QuizLeaderboard {
                 HttpResponse<String> response = client.send(getRequest, HttpResponse.BodyHandlers.ofString());
                 String jsonResponse = response.body();
 
-                // Step B: Extract data and Deduplicate
-                // Using Regex to extract values since we aren't using external JSON libraries
+               
                 Pattern pattern = Pattern.compile("\\{\"roundId\":\"(.*?)\",\"participant\":\"(.*?)\",\"score\":(\\d+)\\}");
                 Matcher matcher = pattern.matcher(jsonResponse);
 
@@ -53,29 +52,29 @@ public class QuizLeaderboard {
 
                     String uniqueKey = roundId + "_" + participant;
 
-                    // Only process if we haven't seen this exact round/participant combo before
+                   
                     if (!seenEvents.contains(uniqueKey)) {
                         seenEvents.add(uniqueKey);
 
-                        // Add score to the participant's total
+                        
                         totalScores.put(participant, totalScores.getOrDefault(participant, 0) + score);
                     }
                 }
 
-                // Mandatory 5-second delay between polls
+               
                 if (i < 9) {
                     Thread.sleep(5000);
                 }
             }
 
-            // Step C: Generate and sort the leaderboard
+           
             System.out.println("\nPolling complete. Calculating leaderboard...");
             List<Map.Entry<String, Integer>> leaderboardList = new ArrayList<>(totalScores.entrySet());
 
-            // Sort in descending order based on score
+            
             leaderboardList.sort((a, b) -> b.getValue().compareTo(a.getValue()));
 
-            // Construct the JSON payload for submission manually
+           
             StringBuilder jsonPayload = new StringBuilder();
             jsonPayload.append("{\n");
             jsonPayload.append("  \"regNo\": \"").append(REG_NO).append("\",\n");
@@ -94,7 +93,7 @@ public class QuizLeaderboard {
 
             System.out.println("\nPayload to submit:\n" + jsonPayload.toString());
 
-            // Step D: Submit the final leaderboard
+           
             HttpRequest postRequest = HttpRequest.newBuilder()
                     .uri(URI.create(BASE_URL + "/quiz/submit"))
                     .header("Content-Type", "application/json")
